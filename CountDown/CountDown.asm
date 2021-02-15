e25449caa309dc95b9ddb15a361462614efde0a4
Adam Carter <mail@adam-carter.com>
2021-02-15 15:46:16 -0800

    finished count down script.

--- CountDown/CountDown.asm
+++ CountDown/CountDown.asm
@@ -7,4 +7,30 @@
 //R3 = 2
 //R4 = 1
 //R5 = 0
-@i
+
+@R0
+M=M-1
+
+//this loop counts up from the last register and works towards R1
+(:loop_begin)
+
+    //go to last register, read value from last + 1 register
+    //store 1 more than that value.  Assumes last + 1 register is set to 0
+    @R0
+    A=M
+    A=A+1
+    D=M
+    @R0
+    A=M
+    M=D+1
+
+    //decrement counter
+    @R0
+    D=M-1
+    M=D
+    @:loop_begin
+    D;JGT
+
+(:end_of_program)
+    @:end_of_program
+    0;JMP
