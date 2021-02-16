fe50dcf49d39fa2e0280b8cf45a0f9f89630955c
Adam Carter <mail@adam-carter.com>
2021-02-15 16:18:15 -0800

    pressing right key now "paints" the screen.

--- KeyPaint/KeyPaint.asm
+++ KeyPaint/KeyPaint.asm
@@ -1,5 +1,8 @@
 //use the keyboard to "paint" our screen
 
+//key_right = 132
+//left_key = 130
+
 @last_key_press
 @x_pos //defines x position of our paint cursor
 @y_pos //defines y position of our paint cursor
@@ -13,9 +16,22 @@ D=M
 @last_key_press
 M=D
 
+//if right key was pressed, increment X pos
+@132
+D=D-A
+@:paint_screen
+D;JNE
+
+//this code only executes when right key was pressed
+@x_pos
+M=M+1
+
 //paint at key
+(:paint_screen)
 @SCREEN
-A=A+D
+D=A
+@x_pos
+A=D+M
 M=-1
 
 //IF D == 128, then end program
